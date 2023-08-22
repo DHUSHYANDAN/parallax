2412f2a31ef0473e8c9051dbf6cc565f7b5fb893
DHUSHYANDAN <dhushyandan302002@gmail.com>
2023-08-22 20:43:02 +0530

    changed

--- jsp.js
+++
@@ -1,9 +0,0 @@
-let text = document.getElementById("text");
-let imgleft = document.getElementById("imgleft");
-let imgright = document.getElementById("imgright");
-
-window.addEventListener('scroll', () => {
-    let Value = window.scrollY;
-   // text.style.margintop= value * 2.5 + 'px';
-    imgleft.style.left = Value * -1.5 + 'px';
-});
